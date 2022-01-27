621476ff908947862273fe80f0927d5328281e08
Joseph Collard <jcollard@users.noreply.github.com>
2022-01-27 04:41:03 -0800

    chore: Add examples.

--- Program.cs
+++ Program.cs
@@ -8,5 +8,15 @@ namespace RandomNameGenerator
         {
             Console.WriteLine("Hello World!");
         }
+
+        // TODO(jcollard 2022-01-27): Add documentation comment
+        public static int GetPositiveInt(string prompt)
+        {
+            // TODO(jcollard 2022-01-27): Add step by step instructions for this method
+            return -1;
+        }
+
+        // TODO(jcollard 2022-01-27): Add remaining method stubs
+
     }
 }
