3b1dc3e60185d2ba59119291e88488e352278c23
willpadian <wpadian2023@chadwickschool.org>
2022-02-03 12:34:08 -0800

    test methods

--- TestGetPositiveInt.cs
+++ TestGetPositiveInt.cs
@@ -8,11 +8,23 @@ namespace RandomNameGenerator
         public static bool RunTest()
         {
             int result = Program.GetPositiveInt("17");
-            if (result != 17) 
+            if (result != 17)
             {
                 Console.Error.WriteLine("The first file should have been 17");
-                return false; 
+                return false;
             }
+
+            int result2 = Program.GetPositiveInt("-5");
+            Console.Error.WriteLine("You must enter a positive integer");
+            return false;
+
+            int result3 = Program.GetPositiveInt("7.6");
+            Console.Error.WriteLine("You must enter a positive integer");
+            return false;
+
+            int result4 = Program.GetPositiveInt("0");
+            Console.Error.WriteLine("You must enter a positive integer");
+            return null;
 
 
             return false;
